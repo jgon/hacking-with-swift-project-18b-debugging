1073e8918ebb6a96c5f191f768414fa324e9a5fe
JackG <jacques.gonzalez@me.com>
2016-02-20 19:00:50 +0100

    Added topics covered.

--- Project18b/ViewController.swift
+++ Project18b/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        for i in 1 ... 100 {
+            print("Got number \(i)")
+        }
     }
 
     override func didReceiveMemoryWarning() {
---
+++ README.md
@@ -0,0 +1,8 @@
+# Hacking with Swift - Project 18b - Debugging
+
+## Topics covered
+
+- ```assert``` statements force the code to crash, but they are disabled in released code.
+- Using conditional breakpoints.
+- Defining breakpoints when an exception is thrown.
+- Using ```View debugging``` with ```Show View Frames```.
